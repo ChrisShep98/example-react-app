84fec05faa7354825a30736bc2edb03121c5f42a
ChrisShep98 <shepardchristopher1@gmail.com>
2022-12-22 21:00:36 -0500

    removed some unnecessary code

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { useTheme, useThemeUpdate } from "./ThemeContext"
+import React from "react";
+import { useTheme } from "./ThemeContext";
 
-const Button = ({text, onClick}) => {
-  const darkTheme = useTheme()
-    const toggleTheme = useThemeUpdate()
-    const themeStyles = {
-        backgroundColor: darkTheme ? '#333' :'#FFFFFF',
-        color: darkTheme ? '#FFFFFF' : '#333'
-    }
+const Button = ({ text, onClick }) => {
+  const darkTheme = useTheme();
+  const themeStyles = {
+    backgroundColor: darkTheme ? "#333" : "#FFFFFF",
+    color: darkTheme ? "#FFFFFF" : "#333",
+  };
   return (
-    <button style={themeStyles} className='bg-white hover:bg-gray-100 text-gray-800 font-semibold px-3 border border-gray-400 rounded shadow h-8' type="button" name="button" onClick={onClick}>{text}</button>
-  )
-}
+    <button
+      style={themeStyles}
+      className="bg-white hover:bg-gray-100 text-gray-800 font-semibold px-3 border border-gray-400 rounded shadow h-8"
+      type="button"
+      name="button"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- src/components/ContextComponent.js
+++ src/components/ContextComponent.js
@@ -5,10 +5,10 @@ import { useTheme, useThemeUpdate } from "./ThemeContext"
 const ContextComponent = () => {
     const darkTheme = useTheme()
     const toggleTheme = useThemeUpdate()
-    const themeStyles = {
-        backgroundColor: darkTheme ? '#333' :'#CCC',
-        color: darkTheme ? '#CCC' : '#333'
-    }
+    // const themeStyles = {
+    //     backgroundColor: darkTheme ? '#333' :'#CCC',
+    //     color: darkTheme ? '#CCC' : '#333'
+    // }
   return (
     <div>
         <Button onClick={toggleTheme} text={'ToggleTheme'}></Button>
