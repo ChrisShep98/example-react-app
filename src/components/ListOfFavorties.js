5e5ce1e7b1e0ad4c87d96e68e75b2ea0e9c26374
ChrisShep98 <shepardchristopher1@gmail.com>
2023-01-30 21:29:34 -0500

    added more props to ListOfFavorites comp

--- src/components/ListOfFavorties.js
+++ src/components/ListOfFavorties.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 // might want to add picture along with title in props but just doing title for now
 
-function ListOfFavorties({photo}) {
+function ListOfFavorties({photo, name, linkToPic}) {
   return (
-    <img src={photo} className="w-64 h-64 p-2" alt=''></img>
+    <div>
+      <a href={linkToPic}><img src={photo} className="w-64 h-64 p-2" alt=''></img></a>
+      <span className='flex justify-center text-sm'>{name}</span>
+    </div>
   )
 }
 
